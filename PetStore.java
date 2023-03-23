1b39d42bcb445a21aeecaa66b7782e84d031d84f
Jessica Jiang <jessicaj2947@gmail.com>
2023-03-24 09:33:35 +1300

    added a searchforpet method that will return the pet if it is the animal type that was searched for

--- PetStore.java
+++ PetStore.java
@@ -1,22 +1,37 @@
+import java.util.ArrayList;
 
 public class PetStore {
 
-    private String shopName;
-    private double cashBalance;
+  private String shopName;
+  private double cashBalance;
 
-    public PetStore(String shopName, double initialBalance) {
-        this.shopName = shopName;
-        this.cashBalance = initialBalance;
+  private ArrayList<Pet> Pets = new ArrayList<Pet>();
+
+  public PetStore(String shopName, double initialBalance) {
+    this.shopName = shopName;
+    this.cashBalance = initialBalance;
+  }
+
+  public String getShopName() {
+    return shopName;
+  }
+
+  public double getCashBalance() {
+    return cashBalance;
+  }
+
+  public int getNumPets() {
+    return Pets.size();
+  }
+
+  public Pet searchForPet(String animal) {
+    for (Pet pet : Pets) {
+      String petLower = pet.getPet().toLowerCase();
+      animal = animal.toLowerCase();
+      if (petLower.contains(animal)) {
+        return pet;
+      }
     }
-
-    public String getShopName() {
-        return shopName;
-    }
-
-    public double getCashBalance() {
-        return cashBalance;
-    }
-
-
-
+    return null;
+  }
 }
