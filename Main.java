327ce310870e48c5f4941630d9871ecc7caf5d41
Jessica Jiang <jessicaj2947@gmail.com>
2023-03-24 10:04:50 +1300

    added new instances of subclasses to main class

--- Main.java
+++ Main.java
@@ -3,10 +3,16 @@ class Main {
 
 	public static void main(String[] args) {
 
-		// Create BookSeller instance and print details
+		// Create pet shop instance and print details
 		PetStore shop = new PetStore("Littlest Pet Shop", 100.0); 
-		System.out.println("Starting balance for " + shop.getShopName() + " is $ " +shop.getCashBalance()); 
+		System.out.println("Starting balance for " + shop.getShopName() + " is $" +shop.getCashBalance()); 
 		System.out.println();
 	}
+
+	Rabbit lola = new Rabbit("Lola", "Bunny", "Dwarf Lop", "3", Pet.Gender.FEMALE);
+    Cat garfield = new Cat("Garfield", "Cat", "Domestic Shorthair", "10", Pet.Gender.MALE);
+	Bird tweety = new Bird("Tweety", "Bird", "Budgie", "2", Pet.Gender.MALE);
+
+	
 
 }
