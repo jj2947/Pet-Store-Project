71e21a8471176fb19e72c6927aee6b3cc3752904
Jessica Jiang <jessicaj2947@gmail.com>
2023-03-24 10:50:47 +1300

    added a statement to sell a pet and then print the inventory details afterwards

--- Main.java
+++ Main.java
@@ -1,30 +1,35 @@
-
 class Main {
 
-	public static void main(String[] args) {
+  public static void main(String[] args) {
 
-		// Create pet shop instance and print details
-		PetStore shop = new PetStore("Littlest Pet Shop", 100.0); 
-		System.out.println("Starting balance for " + shop.getShopName() + " is $" +shop.getCashBalance()); 
-		System.out.println();
-	
+    // Create pet shop instance and print details
+    PetStore shop = new PetStore("Littlest Pet Shop", 100.0);
+    System.out.println(
+        "Starting balance for " + shop.getShopName() + " is $" + shop.getCashBalance());
+    System.out.println();
 
-	Rabbit lola = new Rabbit("Lola", "Bunny", "Dwarf Lop", "3", Pet.Gender.FEMALE);
+    Rabbit lola = new Rabbit("Lola", "Bunny", "Dwarf Lop", "3", Pet.Gender.FEMALE);
     Cat garfield = new Cat("Garfield", "Cat", "Domestic Shorthair", "10", Pet.Gender.MALE);
-	Bird tweety = new Bird("Tweety", "Bird", "Budgie", "2", Pet.Gender.MALE);
+    Bird tweety = new Bird("Tweety", "Bird", "Budgie", "2", Pet.Gender.MALE);
 
-	shop.purchasePet(lola);
+    shop.purchasePet(lola);
     shop.purchasePet(garfield);
     shop.purchasePet(tweety);
-	shop.purchasePet(tweety);
+    shop.purchasePet(tweety);
 
-	System.out.println(
-        "Balance for "
-            + shop.getShopName()
-            + " after buying pets is $"
-            + shop.getCashBalance());
-	
+    System.out.println(
+        "Balance for " + shop.getShopName() + " after buying pets is $" + shop.getCashBalance());
 
-}
+    // Try to sell a Pet
+    boolean success = shop.sellPet("Tweety");
+    if (success) {
+      System.out.println("Sold pet: " + tweety.getName());
+    } else {
+      System.out.println("Could not sell: " + tweety.getName());
+    }
 
+    // Print inventory details
+    shop.printInventoryDetails();
+    System.out.println();
+  }
 }
