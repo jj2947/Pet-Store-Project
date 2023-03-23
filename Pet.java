7934c281058fb2cf21421af2eb714a9de2ee65d1
Jessica Jiang <jessicaj2947@gmail.com>
2023-03-24 09:04:25 +1300

    added constructor to abstract class Pet

--- Pet.java
+++ Pet.java
@@ -1,5 +1,23 @@
 
-public class Pet {
+public abstract class Pet {
+
+    protected enum Gender {FEMALE, MALE}
+    protected String name;
+    protected String age;
+    protected String animal;
+    protected String breed;
+    protected Gender gender;
+
+    public Pet (String name, String animal, String breed, String age, Gender gender) {
+        this.name = name;
+        this.animal = animal;
+        this.age = age;
+        this.gender = gender;
+        this.breed = breed;
+    }
+
+    
+
 
 
 }
