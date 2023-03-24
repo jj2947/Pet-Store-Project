f8ad0ff0a41f76a6ca46ed3760eb99d5d182b468
Jessica Jiang <jessicaj2947@gmail.com>
2023-03-24 15:11:30 +1300

    changed the print statement printing out the cash balance to printing out the inventory which includes cash balance

--- Main.java
+++ Main.java
@@ -8,22 +8,21 @@ class Main {
         "Starting balance for " + shop.getShopName() + " is $" + shop.getCashBalance());
     System.out.println();
 
-	// Create some pet instance
+    // Create some pet instance
     Rabbit lola = new Rabbit("Lola", "Bunny", "Dwarf Lop", "3", Pet.Gender.FEMALE);
     Cat garfield = new Cat("Garfield", "Cat", "Domestic Shorthair", "10", Pet.Gender.MALE);
     Bird tweety = new Bird("Tweety", "Bird", "Budgie", "2", Pet.Gender.MALE);
-	Dog sam = new Dog("Sam", "Dog", "German Shepherd", "4", Pet.Gender.MALE);
+    Dog sam = new Dog("Sam", "Dog", "German Shepherd", "4", Pet.Gender.MALE);
+    Bird bigBird = new Bird("Big Bird", "Bird", "Rainbow Lorikeet", "6", Pet.Gender.FEMALE);
 
-	// Buy some pets for the shop
+    // Buy some pets for the shop
     shop.purchasePet(lola);
     shop.purchasePet(garfield);
     shop.purchasePet(tweety);
     shop.purchasePet(tweety);
-	shop.purchasePet(sam);
+    shop.purchasePet(sam);
 
-	// Print out cash balance of shop
-    System.out.println(
-        "Balance for " + shop.getShopName() + " after buying pets is $" + shop.getCashBalance());
+    shop.printInventoryDetails();
 
     // Personalities of all the pets in the shop
     shop.printPetDetails();
@@ -31,9 +30,11 @@ class Main {
     // Try to sell a Pet
     shop.sellPet("Tweety");
 
-   shop.sellPet("Garfield");
+    shop.sellPet("Garfield");
 
     shop.sellPet("Lola");
+
+	shop.purchasePet(bigBird);
 
     // Print inventory details
     shop.printInventoryDetails();
